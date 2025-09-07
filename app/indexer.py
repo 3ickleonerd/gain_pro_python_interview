2d8fceb778395f0e4b55ef8850bc5e1e42b8cc65
Mehraz <info@mehrazplus.com>
2025-09-07 18:08:12 +0000

    little fix 25

--- app/indexer.py
+++ app/indexer.py
@@ -1,11 +1,12 @@
 import app.helper as helper
 import os
 from dotenv import load_dotenv
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, exceptions
 from elasticsearch import helpers
 from sentence_transformers import SentenceTransformer
 import json
 import logging
+import time
 
 logger = logging.getLogger("uvicorn.error")
 if not logger.hasHandlers():
@@ -23,7 +24,7 @@ def index_if_needed():
 
     client = Elasticsearch(
         str(es_host),
-        # ca_certs=str(crt_path),
+        ca_certs=str(crt_path),
         basic_auth=("elastic", str(es_pass)),
         request_timeout=600,
         verify_certs=False
@@ -32,6 +33,52 @@ def index_if_needed():
     if client.indices.exists(index=str(es_index)):
         # client.indices.delete(index=str(es_index), ignore_unavailable=True)
         return
+    
+    try:
+        client.inference.delete(inference_id="my-elser-endpoint")
+    except exceptions.NotFoundError:
+        # Inference endpoint does not exist
+        pass
+
+    try:
+        client.options(
+            request_timeout=60, max_retries=3, retry_on_timeout=True
+        ).inference.put(
+            task_type="sparse_embedding",
+            inference_id="my-elser-endpoint",
+            body={
+                "service": "elser",
+                "service_settings": {"num_allocations": 1, "num_threads": 1},
+            },
+        )
+        logger.info("Inference endpoint created successfully")
+    except exceptions.BadRequestError as e:
+        if e.error == "resource_already_exists_exception":
+            logger.info("Inference endpoint created successfully")
+        else:
+            raise e
+        
+    inference_endpoint_info = client.inference.get(inference_id="my-elser-endpoint")
+    model_id = inference_endpoint_info["endpoints"][0]["service_settings"]["model_id"]
+
+    while True:
+        status = client.ml.get_trained_models_stats(
+            model_id=model_id,
+        )
+
+        deployment_stats = status["trained_model_stats"][0].get("deployment_stats")
+        if deployment_stats is None:
+            logger.info("ELSER Model is currently being deployed.")
+            time.sleep(5)
+            continue
+
+        nodes = deployment_stats.get("nodes")
+        if nodes is not None and len(nodes) > 0:
+            logger.info("ELSER Model has been successfully deployed.")
+            break
+        else:
+            logger.info("ELSER Model is currently being deployed.")
+        time.sleep(5)
 
     mappings = {}
 
